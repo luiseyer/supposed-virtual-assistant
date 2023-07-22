d94634f38e9b657b68a1477ea35d0841ef925b73
Luis Fernando Reyes <luisfernandorp2001@gmail.com>
2023-07-21 22:32:36 -0400

    add 'basicSsl' for working with https connection

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [basicSsl(), react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
