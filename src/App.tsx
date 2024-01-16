10a4ac4f7ee9eb67117dc26a43949bee52a4507f
luiseyer <luisfernandorp2001@gmail.com>
2024-01-16 17:25:47 -0400

    update base ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { RiRobot2Fill } from '@remixicon/react'
 
 const App: React.FC = () => {
   const [color, setColor] = useState(0)
-  const colors = ['text-teal-500', 'text-green-500', 'text-yellow-500', 'text-pink-500', 'text-red-500']
+  const colors = ['text-yellow-400', 'text-teal-400', 'text-green-400', 'text-pink-400', 'text-red-400']
 
   const changeColor = (): void => {
     setColor(color => {
@@ -17,17 +17,17 @@ const App: React.FC = () => {
 
   return (
     <NextUIProvider>
-      <div className='flex min-h-dvh gap-6 p-6'>
+      <div className='flex min-h-dvh'>
         <Sidebar >
           <SideMenu />
         </Sidebar>
-        <main className='flex grow items-center justify-center gap-4 rounded-2xl bg-default-200 p-6'>
+        <main className='flex grow items-center justify-center gap-4 bg-default-900 p-6'>
           <h1 className='text-3xl font-bold'>
-          Supposed Virtual Assistant
+            Supposed Virtual Assistant
           </h1>
           <Tooltip content='Click to change color'>
             <Button isIconOnly variant='light' className='animate-bounce' onClick={changeColor}>
-              <RiRobot2Fill size={32} className={colors[color]}/>
+              <RiRobot2Fill size={32} className={colors[color]} />
             </Button>
           </Tooltip>
         </main>
--- src/layouts/sidebar.tsx
+++ src/layouts/sidebar.tsx
@@ -4,7 +4,7 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 
 const Sidebar: React.FC<Props> = ({ children, className, ...props }) => {
   return (
-    <aside className={`basis-64 ${className}`} {...props}>
+    <aside className={`basis-72 bg-default-800 p-4 ${className}`} {...props}>
       {children}
     </aside>
   )
