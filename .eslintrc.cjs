ed07f701c84307114f1b09b32742b1448d83d156
luiseyer <luisfernandorp2001@gmail.com>
2024-01-16 11:35:14 -0400

    migrate to TypeScript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,37 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     node: true
   },
   extends: [
-    'standard',
+    'standard-with-typescript',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:tailwindcss/recommended'
   ],
-  plugins: ['react', 'react-hooks', 'react-refresh', 'oxlint'],
+  plugins: ['react', 'react-hooks', 'react-refresh'],
   overrides: [
     {
       env: { node: true },
-      files: ['.eslintrc.{js,cjs}'],
+      files: ['.eslintrc.cjs'],
       parserOptions: { sourceType: 'script' }
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname
   },
   rules: {
     indent: ['error', 2],
     'jsx-quotes': ['error', 'prefer-single'],
     'import/no-absolute-path': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/self-closing-comp': 'error',
     'react-refresh/only-export-components': 'warn'
   }
 }
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@components': `${path.resolve(__dirname, './src/components/')}`,
+      '@layouts': `${path.resolve(__dirname, './src/layouts/')}`
+    }
+  }
+})
