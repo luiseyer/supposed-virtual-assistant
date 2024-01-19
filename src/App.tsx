2fec072ab8de8fc049c5e9aa5a50822da9e9c89f
luiseyer <luisfernandorp2001@gmail.com>
2024-01-19 12:16:07 -0400

    update base ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, Input, Navbar, NavbarBrand, NavbarContent, NavbarItem, NextUIProvider, Textarea, Tooltip } from '@nextui-org/react'
+import { Avatar, Button, Card, CardBody, CircularProgress, Input, Navbar, NavbarBrand, NavbarContent, NavbarItem, NextUIProvider, Textarea, Tooltip } from '@nextui-org/react'
 import { SideMenu } from '@components'
 import { Sidebar } from '@layouts'
 import { RiMore2Line, RiNotification4Line, RiRobot2Fill, RiSearch2Line, RiSendPlane2Line } from '@remixicon/react'
@@ -7,7 +7,6 @@ import { RiMore2Line, RiNotification4Line, RiRobot2Fill, RiSearch2Line, RiSendPl
 const App: React.FC = () => {
   const [color, setColor] = useState(0)
   const colors = ['text-yellow-400', 'text-blue-400', 'text-pink-400', 'text-green-400', 'text-purple-400']
-
   const changeColor = (): void => {
     setColor(color => {
       if (color < colors.length - 1) return color + 1
@@ -15,13 +14,44 @@ const App: React.FC = () => {
     })
   }
 
+  const messages = [
+    {
+      user: 'Hello. How are you?',
+      system: "Hi! I'm fine, thanks for asking. Can I help you with anything?"
+    },
+    {
+      user: 'What is the weather in Spain? ',
+      system: 'The current weather in Spain is partially cloudy with a high of 56°F and a low of 37°F.'
+    },
+    {
+      user: 'What is the square root of 56?',
+      system: 'The square root of 56 is approximately 7.48331477355.'
+    },
+    {
+      user: 'Write a poem',
+      system: null
+    }
+  ]
+
+  const [value, setValue] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setValue(event.target.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent): void => {
+    event.preventDefault()
+    console.log(value)
+  }
+
   return (
     <NextUIProvider>
-      <div className='flex min-h-dvh'>
+      <div className='flex max-h-dvh min-h-dvh'>
         <Sidebar >
           <SideMenu />
         </Sidebar>
-        <main className='flex grow flex-col gap-4 bg-default-900 p-6 pb-24 font-bold'>
+
+        <main className='flex grow flex-col gap-4 bg-default-900 p-6 pb-24'>
           <Navbar
             position='static'
             classNames={{
@@ -65,7 +95,26 @@ const App: React.FC = () => {
               </NavbarItem>
             </NavbarContent>
           </Navbar>
-          <div className='flex h-full items-center justify-center gap-4'>
+
+          <section className='flex h-full snap-y flex-col gap-8 overflow-y-auto p-8'>
+            {messages.map(({ user, system }, i) => (
+              <div key={i} className='max-w-4xl snap-end'>
+                <p className='mb-8 text-default-300'>{user}</p>
+                <div className='flex items-center gap-4'>
+                  <Avatar className='shrink-0' />
+                  <Card shadow='sm' className='bg-default-800 text-default-300'>
+                    <CardBody>
+                      {system === null
+                        ? <CircularProgress size='sm' aria-label='Loading...' />
+                        : <p>{system}</p>}
+                    </CardBody>
+                  </Card>
+                </div>
+              </div>
+            ))}
+          </section>
+
+          <div className='hidden h-full items-center justify-center gap-4'>
             <h1 className='text-3xl font-bold'>
               Supposed Virtual Assistant
             </h1>
@@ -75,18 +124,22 @@ const App: React.FC = () => {
               </Button>
             </Tooltip>
           </div>
+
           <div className='fixed bottom-6 left-0 flex w-full'>
             <div className='shrink-0 basis-72 px-4' />
-            <div className='flex grow items-end gap-4 px-6'>
+            <form onSubmit={handleSubmit} className='flex grow items-end gap-4 px-6'>
               <Textarea
+                value={value}
+                onChange={handleChange}
                 minRows={1}
                 maxRows={20}
                 placeholder='type here...'
+                size='lg'
                 classNames={{
                   input: 'bg-transparent',
                   innerWrapper: 'bg-transparent',
                   inputWrapper: [
-                    'min-h-10',
+                    'min-h-12',
                     'bg-default-800',
                     'group-data-[hover=true]:bg-default-700',
                     'group-data-[focus=true]:bg-default-700',
@@ -94,10 +147,10 @@ const App: React.FC = () => {
                   ]
                 }}
               />
-              <Button color='primary' isIconOnly>
+              <Button color='primary' type='submit' size='lg' isIconOnly>
                 <RiSendPlane2Line />
               </Button>
-            </div>
+            </form>
           </div>
         </main>
       </div>
