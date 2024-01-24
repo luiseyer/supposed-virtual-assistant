8fdd187a09107a53cc7c3e3f0213d8d3435124c4
luiseyer <luisfernandorp2001@gmail.com>
2024-01-24 16:58:32 -0400

    improve Props and extract Tailwind ClassNames

--- src/components/side-menu-item.tsx
+++ src/components/side-menu-item.tsx
@@ -1,13 +1,26 @@
-import { Button } from '@nextui-org/react'
+import { Button, ButtonProps } from '@nextui-org/react'
 import { Link } from '@tanstack/react-router'
+import { clsx } from 'clsx'
 
-interface Props {
-  children: React.ReactNode
-  icon: React.ReactNode
+interface Props extends ButtonProps {
   route: string
+  icon: React.ReactNode
 }
 
-export const SideMenuItem: React.FC<Props> = ({ icon, children, route, ...props }) => {
+const styles = [
+  'group justify-start py-8',
+  '[&_svg]:fill-default-400 [&_svg]:transition-colors [&_svg]:hover:fill-primary',
+  '[&.active]:bg-[hsl(var(--nextui-default)/0.4)]',
+  '[&.active_svg]:fill-primary',
+]
+
+export const SideMenuItem: React.FC<Props> = ({
+  route,
+  icon,
+  className,
+  children,
+  ...props
+}) => {
   return (
     <Button
       as={Link}
@@ -16,7 +29,7 @@ export const SideMenuItem: React.FC<Props> = ({ icon, children, route, ...props 
       size='lg'
       activeProps={{ className: 'active' }}
       startContent={icon}
-      className='group justify-start py-8 [&.active_svg]:fill-primary [&_svg]:fill-default-400 [&_svg]:transition-colors [&_svg]:hover:fill-primary [&.active]:bg-[hsl(var(--nextui-default)/0.4)]'
+      className={clsx(styles, className)}
       {...props}
     >
       {children}
