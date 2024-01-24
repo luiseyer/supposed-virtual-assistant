89c2aced749d17d889a045f0696f73ba4a3e589b
luiseyer <luisfernandorp2001@gmail.com>
2024-01-24 16:55:35 -0400

    extract classNames as constant

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,23 +1,25 @@
 import { Input } from '@nextui-org/react'
 import { RiSearch2Line } from '@remixicon/react'
 
+const classNames = {
+  input: 'bg-transparent',
+  innerWrapper: 'bg-transparent',
+  inputWrapper: [
+    'h-10',
+    'bg-default-800',
+    'group-data-[hover=true]:bg-default-700',
+    'group-data-[focus=true]:bg-default-700',
+    'cursor-pointer',
+  ],
+}
+
 export const SearchInput: React.FC = () => {
   return (
     <Input
       type='text'
       placeholder='search'
       size='sm'
-      classNames={{
-        input: 'bg-transparent',
-        innerWrapper: 'bg-transparent',
-        inputWrapper: [
-          'h-10',
-          'bg-default-800',
-          'group-data-[hover=true]:bg-default-700',
-          'group-data-[focus=true]:bg-default-700',
-          'cursor-pointer',
-        ],
-      }}
+      classNames={classNames}
       startContent={<RiSearch2Line size='16' />}
     />
   )
